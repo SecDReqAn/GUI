b4d4d6c1867f0fab83a7bd0569b02b89c78281c3
Tim Bächlr <t.baechle1999@googlemail.com>
2023-05-15 22:50:52 +0200

    Add fxml template for the Assumption-Entry-UI.

--- src/main/java/ApplicationMain.java
+++ src/main/java/ApplicationMain.java
@@ -1,16 +1,17 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ApplicationMain extends Application {
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("AssumptionEntryUI.fxml"));
+        VBox vbox = loader.load();
+
+        Scene scene = new Scene(vbox);
         stage.setScene(scene);
         stage.show();
     }
