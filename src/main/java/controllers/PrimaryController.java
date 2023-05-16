b251a3d9444abe63b838027f51d5a76be4e8755e
Tim Baechle <t.baechle1999@googlemail.com>
2023-05-16 23:21:43 +0200

    Add toy example for controllers and action handling.

---
+++ src/main/java/controllers/PrimaryController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+    @FXML
+    public void handleNewAssumption(){
+        System.out.println("New Assumption please!");
+    }
+
+}
