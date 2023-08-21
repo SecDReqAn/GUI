767e6763d0287db4288542af802631b84cd6fa59
Tim Baechle <t.baechle1999@googlemail.com>
2023-08-21 17:06:05 +0200

    Fix Configuration deserialization and refactor class.

--- src/main/java/general/Configuration.java
+++ src/main/java/general/Configuration.java
@@ -2,33 +2,49 @@ package general;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Configuration implements Cloneable {
     public static class AnalysisResult implements Cloneable {
-        private String title;
-        private String result;
+        /**
+         * The title of the {@link AnalysisResult} (by default the current date and time in the format dd.mm.yy hh:mm:ss).
+         *
+         * <p>
+         * Note: The title attribute uniquely identifies an {@link AnalysisResult} within a {@link Configuration} (i.e., is is strictly forbidden for two {@link AnalysisResult}s to share the same title).
+         * </p>
+         */
+        private @NotNull String title;
+        private @NotNull String result;
 
-        public AnalysisResult(@NotNull String title, @NotNull String result){
+        /**
+         * Default constructor as <b>required</b> for Jackson deserialization.
+         */
+        @SuppressWarnings("unused")
+        private AnalysisResult() {
+            title = result = "";
+        }
+
+        public AnalysisResult(@NotNull String title, @NotNull String result) {
             this.title = title;
             this.result = result;
         }
 
-        public String getTitle() {
+        public @NotNull String getTitle() {
             return title;
         }
 
-        public String getResult() {
+        public @NotNull String getResult() {
             return result;
         }
 
         @Override
-        public AnalysisResult clone() {
+        public @NotNull AnalysisResult clone() {
             try {
                 AnalysisResult clone = (AnalysisResult) super.clone();
 
@@ -42,23 +58,26 @@ public class Configuration implements Cloneable {
         }
     }
 
+    @Nullable
     private String analysisPath;
+    @Nullable
     private String modelPath;
-    // TODO: Fix deserialization bug where the custom comparator cannot be found (a subsequent cast to Comparable obviously fails).
-    private SortedSet<Assumption> assumptions; // Require sorted property for efficient comparison of configurations.
+    @NotNull
+    private Set<Assumption> assumptions;
+    @NotNull
     private Set<AnalysisResult> analysisResults;
 
     public Configuration() {
-        this.assumptions = new TreeSet<>(new Assumption.AssumptionComparator());
+        this.assumptions = new HashSet<>();
         this.analysisResults = new HashSet<>();
     }
 
-    public Configuration(String analysisPath, String modelPath, Set<Assumption> assumptions, Set<AnalysisResult> analysisResults) {
+    public Configuration(@Nullable String analysisPath, @Nullable String modelPath, @NotNull Set<Assumption> assumptions, @NotNull Set<AnalysisResult> analysisResults) {
         this.analysisPath = analysisPath;
         this.modelPath = modelPath;
         this.analysisResults = new HashSet<>();
         this.analysisResults.addAll(analysisResults);
-        this.assumptions = new TreeSet<>(new Assumption.AssumptionComparator());
+        this.assumptions = new HashSet<>();
         this.assumptions.addAll(assumptions);
     }
 
@@ -74,57 +93,67 @@ public class Configuration implements Cloneable {
     public boolean isMissingAnalysisParameters() {
         return this.analysisPath == null ||
                 this.modelPath == null ||
-                this.assumptions == null ||
                 this.getAssumptions().isEmpty();
     }
 
-    public String getAnalysisPath() {
+    public @Nullable String getAnalysisPath() {
         return analysisPath;
     }
 
-    public void setAnalysisPath(String analysisPath) {
+    public void setAnalysisPath(@Nullable String analysisPath) {
         this.analysisPath = analysisPath;
     }
 
-    public String getModelPath() {
+    public @Nullable String getModelPath() {
         return modelPath;
     }
 
-    public void setModelPath(String modelPath) {
+    public void setModelPath(@Nullable String modelPath) {
         this.modelPath = modelPath;
     }
 
-    public Set<Assumption> getAssumptions() {
+    public @NotNull Set<Assumption> getAssumptions() {
         return assumptions;
     }
 
-    public Set<AnalysisResult> getAnalysisResults() {
+    public @NotNull Set<AnalysisResult> getAnalysisResults() {
         return analysisResults;
     }
 
     @Override
     public boolean equals(Object object) {
         if (object instanceof Configuration otherConfiguration) {
-            boolean assumptionsEqual;
-            if (this.assumptions == null || otherConfiguration.assumptions == null) {
-                assumptionsEqual = this.assumptions == otherConfiguration.assumptions;
-            } else {
-                assumptionsEqual = this.assumptions.size() == otherConfiguration.assumptions.size();
+            // Check whether the Assumption instances are semantically identical.
+            if (this.assumptions.size() != otherConfiguration.assumptions.size()) {
+                return false;
+            }
 
-                if (assumptionsEqual) {
-                    var assumptionIteratorThis = this.assumptions.iterator();
-                    var assumptionIteratorOther = otherConfiguration.assumptions.iterator();
-                    while (assumptionIteratorThis.hasNext() && assumptionIteratorOther.hasNext())
-                        if (!assumptionIteratorThis.next().semanticallyEqualTo(assumptionIteratorOther.next())) {
-                            return false;
-                        }
+            ArrayList<Assumption> assumptionsThis = new ArrayList<>(this.assumptions);
+            ArrayList<Assumption> assumptionsOther = new ArrayList<>(otherConfiguration.assumptions);
+            var assumptionComparator = new Assumption.AssumptionComparator();
+            assumptionsThis.sort(assumptionComparator);
+            assumptionsOther.sort(assumptionComparator);
+            for (int i = 0; i < assumptionsThis.size(); i++) {
+                if (!assumptionsThis.get(i).semanticallyEqualTo(assumptionsOther.get(i))) {
+                    return false;
                 }
             }
 
-            return Objects.equals(this.analysisPath, otherConfiguration.analysisPath)
-                    && Objects.equals(this.modelPath, otherConfiguration.modelPath)
-                    && Objects.equals(this.analysisResults, otherConfiguration.analysisResults)
-                    && assumptionsEqual;
+
+            // Check whether AnalysisResult instances are semantically identical (titles are assumed to be unique identifiers).
+            if (this.analysisResults.size() != otherConfiguration.analysisResults.size()) {
+                return false;
+            }
+
+            Set<String> titlesThis = this.analysisResults.stream().map(AnalysisResult::getTitle).collect(Collectors.toSet());
+            // Enforce HashSet on other set for O(1) lookup.
+            HashSet<String> titlesOther = otherConfiguration.analysisResults.stream().map(AnalysisResult::getTitle).collect(Collectors.toCollection(HashSet::new));
+            boolean analysisResultsEqual = titlesThis.stream().filter(title -> !titlesOther.contains(title)).findFirst().isEmpty();
+
+
+            return analysisResultsEqual
+                    && Objects.equals(this.analysisPath, otherConfiguration.analysisPath)
+                    && Objects.equals(this.modelPath, otherConfiguration.modelPath);
         }
 
         return false;
@@ -150,7 +179,7 @@ public class Configuration implements Cloneable {
             clone.analysisResults = new HashSet<>();
             this.analysisResults.forEach(analysisResult -> clone.analysisResults.add(analysisResult.clone()));
 
-            clone.assumptions = new TreeSet<>(new Assumption.AssumptionComparator());
+            clone.assumptions = new HashSet<>();
             this.assumptions.forEach(assumption -> clone.assumptions.add(assumption.clone()));
 
             return clone;
